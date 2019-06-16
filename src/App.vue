e5c3f7c954059295c7c6e11b2767efb476d8c112
dev1203 <devsingh120397@gmail.com>
2019-06-16 11:03:01 -0700

    Initial setup for vuejs

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <LandingPage></LandingPage>
+</template>
+
+<script>
+import LandingPage from './components/landingPage';
+
+export default {
+  name: 'LayoutDefault',
+
+  components: {
+    LandingPage
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop
+    }
+  }
+}
+</script>
+
+<style>
+body{
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+}
+.container{
+    margin-left: 10%;
+    margin-right: 10%;
+}
+</style>
---
+++ src/components/landingPage.vue
@@ -0,0 +1,36 @@
+<template>
+	<div class="row container landing shadow-9">
+		<div class="col-sm-6 col-12 landing__heading">
+			<h3 class=".text-h3 h3">Hello World</h3>
+			<h4 class=".text-h4 h4">I am Dev!</h4>
+		</div>
+		<div class="col-sm-6 col-12 landing__image">
+			<img src="../assets/lazy_programmer_dribbble-02.png" alt="">
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	
+}
+</script>
+
+<style lang="stylus" scoped>
+@import url('https://fonts.googleapis.com/css?family=Dancing+Script&display=swap');
+img 
+	width 100%;
+	max-height 600px;
+
+.landing__heading
+	width 100%;
+	text-align center;
+	margin auto 0;
+	font-family 'Dancing Script', cursive;
+.landing
+	min-height 100%;
+	margin-top auto;
+	margin-bottom auto;
+</style>
+
+
